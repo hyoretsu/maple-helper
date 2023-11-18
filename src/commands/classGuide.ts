14567679face12def3bbb9a727ab601e8230fedb
Aran Leite <hyoretsu@gmail.com>
2023-11-17 23:51:35 -0300

    Added class guide command

---
+++ src/commands/classGuide.ts
@@ -0,0 +1,46 @@
+import { SlashCommandBuilder } from "discord.js";
+import { Command } from "../@types";
+import guides from "../data/guides.json";
+import normalizeClassName from "../utils/normalizeClassName";
+
+const classGuideCommand: Command = {
+	data: new SlashCommandBuilder()
+		.setName("class_guide")
+		.setDescription(
+			"Easy-to-get class guide image. (Contact the developer for missing/broken/outdated guides)",
+		)
+		.addStringOption(option =>
+			option.setName("class").setDescription("You'll get the guide image for this class.").setRequired(true),
+		),
+	async execute(interaction) {
+		try {
+			const chosenClass = normalizeClassName(interaction.options.getString("class") as string);
+
+			// @ts-ignore
+			const guideLink = guides[chosenClass];
+
+			if (!guideLink) {
+				await interaction.reply({
+					content:
+						"Sorry, we don't have the guide for this class yet. Please contact the developer with a (preferrably permanent) link to the guide image.",
+					ephemeral: true,
+				});
+
+				return;
+			}
+
+			await interaction.reply({
+				content: `Here is your guide:\n${guideLink}`,
+				ephemeral: true,
+			});
+		} catch {
+			await interaction.reply({
+				content:
+					"The class you requested either doesn't exist in our database or has a wrong spelling. Please try again or contact the developer.",
+				ephemeral: true,
+			});
+		}
+	},
+};
+
+export default classGuideCommand;
